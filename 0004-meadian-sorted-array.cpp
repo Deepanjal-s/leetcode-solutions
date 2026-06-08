3494a8afbb02a4c67ee42d2418ab32e1a91682c0
Deepanjal Shukla <deepanjalshukla720@gmail.com>
2026-06-09 00:46:42 +0530

    changed question 4th

--- 0004-meadian-sorted-array.cpp
+++ 0004-meadian-sorted-array.cpp
@@ -12,8 +12,8 @@ public:
             return findMedianSortedArrays(nums2,nums1);
         }
 
-        int x = nums1.size() - 1;
-        int y = nums2.size() - 1;
+        int x = nums1.size();
+        int y = nums2.size();
 
         int start = 0;
         int end = x;
@@ -23,17 +23,29 @@ public:
             int party = ((x + y + 1)/2)- partx;
 
             int xleft = (partx == 0) ? INT_MIN : nums1[partx - 1];
-            int xright = (partx == 0) ? INT_MAX : nums1[partx];
+            int xright = (partx == x) ? INT_MAX : nums1[partx];
             int yleft = (party == 0) ? INT_MIN : nums2[party - 1];
-            int yright = (d) 
-            
+            int yright = (party == y) ? INT_MAX : nums2[party];
+
+            if(xleft <= yright && yleft <= xright){
+                if((x+y)%2 == 0){
+                    return ((double) max(xleft,yleft) + min(xright,yright))/2;
+                }else{
+                    return max(xleft,yleft);
+                }
+            }else if(xleft > yright){
+                end = partx - 1;
+            }else{
+                start = partx + 1;
+            }
         }
+        return 0;
     }
 };
 
 int main(){
     vector<int> num1 = {1,2,3,5,6,9};
-    vector<int> num2 = {};
+    vector<int> num2 = {10};
     Solution obj;
     double ans = obj.findMedianSortedArrays(num1,num2);
 
